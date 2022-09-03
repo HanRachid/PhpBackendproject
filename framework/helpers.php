e973fbe31912f5f4d004d4f21373a46a48013876
HanRachid <handaouirachid@gmail.com>
2022-09-03 13:11:05 +0100

    remove engines, not needed as react is used

--- framework/helpers.php
+++ framework/helpers.php
@@ -3,50 +3,29 @@
 use Framework\Validation\Rule\EmailRule;
 use Framework\Validation\Rule\MinRule;
 use Framework\Validation\Rule\RequiredRule;
-use Framework\View\Engine\AdvancedEngine;
-use Framework\View\Engine\BasicEngine;
-use Framework\View\Engine\PhpEngine;
-use Framework\View\Manager;
-use Framework\View\View;
+
 use Framework\Validation;
 use Framework\Validation\Rule\UniqueRule;
 
-if (!function_exists('view')) {
-    function view(string $template, array $data = []): View
+if (!function_exists('redirect')) {
+    function redirect(string $url)
+    {
+        header("Location: {$url}");
+
+        exit;
+    }
+}
+if (!function_exists('validate')) {
+    function validate(array $data, array $rules)
     {
         static $manager;
         if (!$manager) {
-            $manager = new Manager();
-            $manager->addPath(__DIR__.'/../resources/views');
-            $manager->addEngine('basic.php', new BasicEngine());
-            $manager->addEngine('advanced.php', new AdvancedEngine());
-            $manager->addEngine('php', new PhpEngine());
-
-            $manager->addMacro('escape', fn ($value) => htmlspecialchars($value));
-            $manager->addMacro('includes', fn (...$params) => print view(...$params));
+            $manager = new Validation\Manager();
+            $manager->addRule('required', new RequiredRule());
+            $manager->addRule('email', new EmailRule());
+            $manager->addRule('min', new MinRule());
+            $manager->addRule('unique', new UniqueRule());
         }
-        return $manager->resolve($template, $data);
-    }
-    if (!function_exists('redirect')) {
-        function redirect(string $url)
-        {
-            header("Location: {$url}");
-
-            exit;
-        }
-    }
-    if (!function_exists('validate')) {
-        function validate(array $data, array $rules)
-        {
-            static $manager;
-            if (!$manager) {
-                $manager = new Validation\Manager();
-                $manager->addRule('required', new RequiredRule());
-                $manager->addRule('email', new EmailRule());
-                $manager->addRule('min', new MinRule());
-                $manager->addRule('unique', new UniqueRule());
-            }
-            return $manager->validate($data, $rules);
-        }
+        return $manager->validate($data, $rules);
     }
 }
